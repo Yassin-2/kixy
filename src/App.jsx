b8f8bef19e598d10390e69ad4d7fe5c25db67310
Yassin-2 <kanajahtresor@gmail.com>
2026-03-09 13:12:13 +0200

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+          <Router>
+            <Routes path='/' element={<Home />} />
+          </Router>
+      </BrowserRouter>
     </div>
   )
 }
